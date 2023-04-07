92bb70df0c039077e9b3c105ad623a09aded5e75
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-07 10:12:22 +0100

    style: add animations to homepage

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+import { animate } from "motion";
+import './style.css';
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const logo = document.querySelector(".home__logo");
+logo.style.opacity = 0;
 
-// setupCounter(document.querySelector('#counter'))
+animate(
+  logo,
+  { opacity: 1, y:-10 },
+  { delay: 0.2, duration: 1 }
+);
+
+const slogan = document.querySelector(".home__slogan");
+slogan.style.opacity = 0;
+
+animate(
+  slogan,
+  { opacity: 1, y:-10 },
+  { delay: 1.3, duration: 1 }
+);
