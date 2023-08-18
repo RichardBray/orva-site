36316972c3dbf9d361d5fb25c0ece009c5ff25f6
RichardBray <richardbray89@gmail.com>
2023-08-18 21:21:33 +0100

    chore(vite.config.js): add rollupOptions to specify entry points for build process feat(vite.config.js): add support for resolving file paths using node:path module for entry points in rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5000,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        deck: resolve(__dirname, 'deck/index.html')
+      }
+    }
   }
 });
